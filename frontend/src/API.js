a32cdd65847013f72c8b49af496fb73704a971ea
Vikram Hegde <vikramhegde4037@gmail.com>
2025-05-15 14:55:10 +0530

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,8 +1,8 @@
 //api base for local backend
-const API_BASE = "http://localhost:5000";
+// const API_BASE = "http://localhost:5000";
 
 //api base for production render account
-// const API_BASE = "https://faro-vb8w.onrender.com";
+const API_BASE = "https://faro-vb8w.onrender.com";
 
 //api base for testing render account
 // const API_BASE = "https://faro-1.onrender.com";
