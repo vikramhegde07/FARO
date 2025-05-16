beec08ebaca7fe79a4070012516e3782f60096c3
Vikram Hegde <vikramhegde07@gmail.com>
2025-05-16 19:51:29 +0530

    Major changes to event management the event content can now be built with builder

--- frontend/src/Pages/Event.jsx
+++ frontend/src/Pages/Event.jsx
@@ -30,7 +30,7 @@ function Event() {
             <div className="row gap-3">
                 {events.length === 0 ? '' :
                     events.map((event) => (
-                        <div className="col-md-3" key={evevnt._id}>
+                        <div className="col-md-3" key={event._id}>
                             <div className="card shadow">
                                 <div className="card-header flex-jbetween">
                                     <p className="card-text mb-0">Upcoming </p>
@@ -39,7 +39,7 @@ function Event() {
                                 <div className="card-body">
                                     <h5 className="card-title fw-semibold">{event.title}</h5>
                                     <div className="position-relative overflow-hidden" style={{ height: "150px", textAlign: 'justify' }}>
-                                        <p className="card-text">{event.desc}</p>
+                                        <p className="card-text"></p>
                                         <div className="position-absolute top-0 left-0 w-100 h-100 bg-gradient-trans"></div>
                                     </div>
                                     <Link to={`/event/${event._id}`} className="btn btn-danger px-4 rounded-0">View Details</Link>
